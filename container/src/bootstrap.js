fbe233a1320bbcaf2554e560a886e5a41a993eaa
Morné Louw <louw.morne@gmail.com>
2022-03-31 18:09:16 +0200

    14. Implementing Module Federation

---
+++ container/src/bootstrap.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("products/ProductsIndex");
+console.log('Container');
+//# sourceMappingURL=bootstrap.js.map
--- container/src/index.ts
+++ container/src/index.ts
@@ -1,1 +1,5 @@
-console.log('Container');
+import('./bootstrap').then(_ => {
+    console.log("Hello From Container (index.js)");
+}).catch((reason) => {
+    console.error(`Reason: ${reason}`);
+});
