5bc284d9ac4ec5f6e6e45a71d78993c3330bb3e8
Morné Louw <louw.morne@gmail.com>
2022-04-01 15:29:50 +0200

    26. Refactoring the Products App to more reliably run in Stand-alone vs Consumed modes

--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -3,11 +3,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.mount = void 0;
 const faker_1 = __importDefault(require("faker"));
-let products = '';
-for (let i = 0; i < 3; i++) {
-    const name = faker_1.default.commerce.productName();
-    products += `<div>${name}</div>`;
+const mount = (el) => {
+    let products = '';
+    for (let i = 0; i < 3; i++) {
+        const name = faker_1.default.commerce.productName();
+        products += `<div>${name}</div>`;
+    }
+    document.querySelector(el).innerHTML = products;
+};
+exports.mount = mount;
+if (process.env.NODE_ENV === 'development') {
+    const isStandAlone = !!document.querySelector('body[data-app-name="products"]');
+    if (isStandAlone) {
+        mount('#dev-products');
+    }
 }
-document.querySelector('#dev-products').innerHTML = products;
 //# sourceMappingURL=bootstrap.js.map
--- products/src/bootstrap.ts
+++ products/src/bootstrap.ts
@@ -1,10 +1,24 @@
 import faker from 'faker';
 
-let products = '';
+const mount = (el: string) => {
+    let products = '';
 
-for (let i = 0; i < 3; i++) {
-    const name = faker.commerce.productName();
-    products += `<div>${name}</div>`;
+    for (let i = 0; i < 3; i++) {
+        const name = faker.commerce.productName();
+        products += `<div>${name}</div>`;
+    }
+
+    document.querySelector(el)!.innerHTML = products;
+};
+
+// Check if the app is running in development mode
+// - "process.env.NODE_ENV" is set by webpack baseed on the "mode" property in your relevant webpack config
+if (process.env.NODE_ENV === 'development') {
+    const isStandAlone = !!document.querySelector('body[data-app-name="products"]');
+    if (isStandAlone) {
+        mount('#dev-products');
+    }
 }
 
-document.querySelector('#dev-products')!.innerHTML = products;
+// This export is available to Container apps
+export { mount };
