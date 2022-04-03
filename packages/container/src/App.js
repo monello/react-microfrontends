9097c7e8e6790e7d093926058d87355f1b382684
Morné Louw <louw.morne@gmail.com>
2022-04-03 07:48:13 +0200

    43. Container App - Module Federation - Consume MarketingApp

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import { mount } from 'marketing/MarketingApp';
+
+console.log(mount);
 
 const App = () => {
+    // return mount(document.querySelector('#root'));
     return (
         <div>Hello World</div>
     );
