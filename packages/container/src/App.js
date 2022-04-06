25d15676a3c2c5750064387795bfdea3b5209ffd
Morné Louw <louw.morne@gmail.com>
2022-04-06 17:17:14 +0200

    115. Protecting Access to the Dashboard from logged out users

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,6 +1,7 @@
-import React, { lazy, Suspense, useState } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React, { lazy, Suspense, useState, useEffect } from 'react';
+import { Router, Route, Switch, Redirect } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+import { createBrowserHistory } from 'history';
 
 import Header from './components/Header';
 import { LinearProgress } from '@material-ui/core';
@@ -13,11 +14,28 @@ const generateClassName = createGenerateClassName({
     productionPrefix: 'co'
 });
 
+// So we are switching the creation of the Browser History, to an alternative way of creating it via the generic Router component
+// - Here we craete the history object, but still making sure it's Browser History and not Memory History
+// - This just makes it a lot easier (as per the lecturer) to get access fro the history component on the same page as where we
+//   implement the Browser History
+// - Lower down we then replace the <BrowserRouter> tags with <Router> tags and passing this this history object to it
+// - This will bring as back to Browser History pattern (because the history object was created with "createBrowserHistory()"), but
+//   with the added benefit that we have access to the history object
+const history = createBrowserHistory();
+
 const App = () => {
     const [isSignedIn, setIsSignedIn] = useState(false);
 
+    // Passing the "isSignedIn" state property to the useEffect hook, will make the callback function (1st argument to useEffect)
+    //  run each time isSignedIn changes
+    useEffect(() => {
+        if (isSignedIn) {
+            history.push('/dashboard');
+        }
+    }, [isSignedIn]);
+
     return (
-        <BrowserRouter>
+        <Router history={history}>
             <StylesProvider generateClassName={generateClassName}>
                 <div>
                     <Header isSignedIn={isSignedIn} onSignOut={() => setIsSignedIn(false)} />
@@ -26,13 +44,16 @@ const App = () => {
                             <Route path='/auth'>
                                 <AuthAppLazy onSignIn={() => setIsSignedIn(true)} />
                             </Route>
-                            <Route path='/dashboard' component={DashboardAppLazy} />
+                            <Route path='/dashboard'>
+                                {!isSignedIn && <Redirect to="/" />}
+                                <DashboardAppLazy />
+                            </Route>
                             <Route path='/' component={MarketingAppLazy} />
                         </Switch>
                     </Suspense>
                 </div>
             </StylesProvider>
-        </BrowserRouter>
+        </Router>
     );
 };
 
