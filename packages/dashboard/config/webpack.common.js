b3a4153b479fb2621599ee763955738c876b67d0
Morné Louw <louw.morne@gmail.com>
2022-04-06 13:10:49 +0200

    110. Initial Dashboard Setup

---
+++ packages/dashboard/config/webpack.common.js
@@ -0,0 +1,49 @@
+/**
+ * Webpack Config common to both Development and Production
+ * This will be merged with either `webpack.dev.js` or `webpack.prod.js` depending on whether we are building for Development or Production respectively
+ */
+const { VueLoaderPlugin } = require('vue-loader');
+
+module.exports = {
+    entry: './src/index.jt',
+    output: {
+        filename: '[name].[contenthash].js'
+    },
+    resolve: {
+        extensions: ['.js', '.vue']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpe?g|gif|woff|svg|eot|ttf)$/i,
+                use: [
+                    { loader: 'file-loader' }
+                ]
+            },
+            {
+                test: /\.vue$/,
+                use: 'vue-loader'
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: ['vue-loader', 'style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env'
+                        ],
+                        plugins: [
+                            '@babel/plugin-transform-runtime'
+                        ]
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [new VueLoaderPlugin()]
+};
---
+++ packages/dashboard/src/bootstrap.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
+import App from './App';
+
+const mount = (el, { onNavigate, defaultHistory, initialPath, onSignIn }) => {
+    const history = defaultHistory || createMemoryHistory({
+        initialEntries: [initialPath]
+    });
+
+    // Make sure that the onNavigate callback exists (we don't send it in when we run this app in isolation)
+    if (onNavigate) {
+        history.listen(onNavigate);
+    }
+
+    ReactDOM.render(
+        <App
+            onSignIn={onSignIn}
+            history={history}
+        />,
+        el          // Target element to render the JSX in
+    );
+
+    // We need a way for the Container app to communicate back down to the Child application in a Generic, framework-agnostic way
+    // - We return an object that can become a "handle" for the marketing app inside the Container app in a simple JS object format
+    // - So no React specific tricks here, nice and generic JS object
+    // - This object can therefore containe ANYTHING we want it to, and for now that is a function type property to inform the child app
+    //      when relevant navigation occured inthe container app, so that can be synced down to the "Memory History" used here
+    return {
+        onParentNavigate({ pathname: nextPathname }) {
+            const { pathname } = history.location;
+            if (pathname !== nextPathname) {
+                history.push(nextPathname);
+            }
+        }
+    };
+};
+
+if (process.env.NODE_ENV === 'development') {
+    const isStandAlone = !!document.querySelector('body[data-app-name="auth"]');
+    if (isStandAlone) {
+        // Adding a fix for the expected 2nd argument we added for the onNavigate callback sent by the Container app, but nor needed in Stand-alone mode
+        mount(
+            document.getElementById("app"),
+            { defaultHistory: createBrowserHistory() }
+        );
+    }
+}
+
+export { mount };
